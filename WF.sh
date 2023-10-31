1481664893ac586158b8f7abdfa3f7f71998d51d
Ubuntu <elora95.vigo@gmail.com>
2023-10-31 11:06:27 +0000

    blast

---
+++ WF.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+
+if [ ! -f ncbi-blast-2.14.1+-x64-linux.tar.gz ]
+then
+wget  https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.14.1+-x64-linux.tar.gz
+tar -zxvf ncbi-blast-2.14.1+-x64-linux.tar.gz
+fi
+
+
+if [ ! -d prot ]
+then tar -xvf prot.tar
+fi
+
+if [ ! -d Blast_db ]
+then mkdir Blast_db
+fi
+
+./blast_db_creation.sh
