c2e6fccc016d6ce72d3b2a5ff6aa70d43ba0dd51
Elora-V <elora95.vigo@gmail.com>
2023-11-01 10:28:48 +0000

    fin clean blast

--- main.sh
+++ main.sh
@@ -5,17 +5,16 @@
 ##### PARAMETERS #####
 
 . ./config.sh
-while getopts prot:blast: flag
+
+fileprot="prot.tar" # default value
+
+while getopts p:b: flag
 do
     case "${flag}" in
-    	prot) fileprot=${OPTARG};;
-        blast) resultBlast=${OPTARG};;         
+    	p) fileprot=${OPTARG};;
+        b) resultBlast=${OPTARG};;       
     esac
 done
-
-if [ -z $fileprot ]
-then $fileprot="prot.tar"
-fi
 
 ##### END PARAMETERS #
 
@@ -24,40 +23,77 @@ fi
 
 ##### MAIN #####
 
-# Download blast
+### Download blast ###
+
+echo ""
+echo "#########################################################"  
+echo "Telechargement Blast"
+echo "#########################################################"
+echo ""
+
 if [ ! -f ncbi-blast-2.14.1+-x64-linux.tar.gz ]
 then
-wget  https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.14.1+-x64-linux.tar.gz
-tar -zxvf ncbi-blast-2.14.1+-x64-linux.tar.gz
+	wget  https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.14.1+-x64-linux.tar.gz
+	tar -zxvf ncbi-blast-2.14.1+-x64-linux.tar.gz
+else echo "already done"
 fi
 
-# directory of protein in genomes 
+### Get proteins files ###
+echo ""
+echo "#########################################################"  
+echo "Get proteins file"
+echo "#########################################################"
+echo ""
+
 if [ ! -d $path_to_prot_file ]
-then tar -xvf $fileprot -C $path_to_prot_file
+then 
+	mkdir $path_to_prot_file
+
+	case $fileprot in
+    *.tar|*.tar.gz)
+		# dezip
+        tar -xvf $fileprot 
+        ;;
+    *)
+		# move
+        mv ${fileprot}/* $path_to_prot_file
+        ;;
+	esac
 fi
 
+### Blast ###
+
+echo ""
+echo "#########################################################"  
+echo "Blast"
+echo "#########################################################"
+echo ""
 # directory of genome database
 if [ ! -d $path_to_db ]
 then mkdir $path_to_db
 fi
-
-
 # make database and blast with ncbi
-if [ -z $resultBlast ]
+if [ ! -z $resultBlast ]
 then 
-	# recupère resultat déjà prêt
+	# get result directly in directory
 	if [ ! -d $path_to_blast_out ]
 	then mkdir $path_to_blast_out 
 	fi
-	if if [[ $resultBlast == *.tar ]]
-	then tar -xvf $resultBlast -C $path_to_blast_out
-	else mv $resultBlast $path_to_blast_out
-	fi
+	
+	
+	case $resultBlast in
+    	*.tar|*.tar.gz)
+			# dezip
+			tar -xvf $resultBlast -C $path_to_blast_out ;;
+		*)
+			# move
+			mv $resultBlast $path_to_blast_out ;;
+	esac
 
 else
-	# script qui renvoie les outputs
-	chmod +x ./blast_db_creation.sh
-	chmod +x ./launch_blast.sh
+	# execution script
+	chmod +x ./modules/blast_db_creation.sh
+	chmod +x ./modules/launch_blast.sh
 	./modules/blast_db_creation.sh
 	./modules/launch_blast.sh
 fi
